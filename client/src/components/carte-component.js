a760922c9838c8be39322ad3bb4102e3e5c6997e
Eli Leblanc <eli_blanc@hotmail.com>
2022-08-28 16:16:02 -0300

    changed jokers and tried to fix deployment

--- client/src/components/carte-component.js
+++ client/src/components/carte-component.js
@@ -25,7 +25,7 @@ export class CarteComponent extends Component {
                                     <p style={{ color: this.props.carte.couleur, fontSize: '24px', marginTop: '-5px' }}>
                                         {this.props.carte.symbole}
                                     </p>
-                                    <div style={{ marginTop: this.props.carte.symbole === '' ? '-15px' : '-25px' }}><img alt="oups..." className={this.props.carte.symbole === '' ? 'App-jokers' : 'App-sorte'} src={require(`../assets/images/${this.props.carte.image}`)} /></div>
+                                    <div style={{ marginTop: this.props.carte.symbole === '' ? '-10px' : '-25px' }}><img alt="oups..." className={this.props.carte.symbole === '' ? 'App-jokers' : 'App-sorte'} src={require(`../assets/images/${this.props.carte.image}`)} /></div>
                                 </div>
                             }
                             {
